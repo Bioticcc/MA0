78ab07b7c062c2dc21a9d05c160f18ea4ce7b551
Bioticcc <adamward.bio@gmail.com>
2025-01-06 15:01:21 -0800

    hello world

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>   
+#include <string>      
+#include <vector>    
+#include <map>     
+#include <algorithm>   
+#include <stdexcept>  
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+
+
+int main(void) {
+    cout << "hello world" << endl;
+}
