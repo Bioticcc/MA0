64225ebff3acbb9feaed51d7c61e28c76b4d60f0
Bioticcc <adamward.bio@gmail.com>
2025-01-10 13:17:24 -0800

    MAO hello world

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,21 +1,8 @@
+#include <iostream>
 
-#pragma once
+using namespace std;
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>   
-#include <string>      
-#include <vector>    
-#include <map>     
-#include <algorithm>   
-#include <stdexcept>  
-#include <fstream>
-
-using std::cout;
-using std::cin;
-using std::string;
-
-
-
-int main(void) {
-    cout << "hello world" << endl;
+int main(){
+    cout << "Hello CPTS_223 by Adam Ward" << endl;
+    return 0;
 }
